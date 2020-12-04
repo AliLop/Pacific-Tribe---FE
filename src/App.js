bf33d6cda868a97888b7e37a4cfe21db175ace74
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-12-04 16:07:18 +0000

    moodboard

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import React from 'react';
 
-// See what to remove, this comes from the previous project.
-
-// import AddProject from './components/AddProject';
-// import ProjectDetail from './components/ProjectDetail';
-// import EditProject from './components/EditProject';
-
-// Project 3 imports
 import AuthService from './utils/auth';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Login from './components/Auth/Login';
 import Navbar from './components/Main/Navbar';
 import Signup from './components/Auth/Signup';
-import Home from './components/Main/Homepage';
-import MusicDaily from './components/Suggestions/MusicDaily';
-import YogaVideo from './components/Suggestions/Yoga';
-import MeditationVideo from './components/Suggestions/Meditation';
+import Homepage from './components/Main/Homepage';
 import MoodService from './utils/mapi'
 import DailyMood from './components/Main/DailyMood';
+import Moodboard from './components/Main/Moodboard';
+// import MusicDaily from './components/Suggestions/MusicDaily';
+// import YogaVideo from './components/Suggestions/Yoga';
+// import MeditationVideo from './components/Suggestions/Meditation';
 
 class App extends React.Component {
 
@@ -87,34 +81,7 @@ class App extends React.Component {
         <ToastContainer />
           <Navbar loggedInUser={this.state.loggedInUser} setCurrentUser={this.setCurrentUser}/>
           <Switch>
-            <Route exact path="/" component={Home} />
-            {/* <Route exact path="/music-daily" component={MusicDaily} />
-            <Route exact path="/yoga-video" component={YogaVideo} />
-            <Route exact path="/meditation-video" component={MeditationVideo} /> */}
-            {/* <Route exact path="/daily-mood" component={DailyMood} /> */}
-            
-            <Route exact path="/daily-mood" render={
-              () => {
-                if (localStorage.getItem('loggedInUser')) {
-                  return <DailyMood loggedInUser={this.state.loggedInUser} />
-                } else {
-                  return <Redirect to='/login' />
-              }
-            }
-            }/>
-
-{/* 
-            <Route exact path="/projects/add" render={
-              () => {
-                if (localStorage.getItem('loggedInUser')) {
-                  return <AddProject />
-                } else {
-                  return <Redirect to='/login' />
-              }
-            }
-            }/>
-            <Route exact path="/projects/:id" component={ProjectDetail} />
-            <Route exact path="/projects/:id/edit" component={EditProject} /> */}
+            <Route exact path="/" component={Homepage} />
             <Route path='/signup' component={Signup} />
             <Route path='/login' render={
               () => {
@@ -126,6 +93,21 @@ class App extends React.Component {
                 window.location.href = `${process.env.REACT_APP_PROJECT_API}/api/auth/google`
               }
             } />
+
+            <Route exact path="/daily-mood" render={
+              () => {
+                if (localStorage.getItem('loggedInUser')) {
+                  return <DailyMood loggedInUser={this.state.loggedInUser} />
+                } else {
+                  return <Redirect to='/login' />
+              }
+            }
+            }/>
+            <Route exact path="/moodboard" component={Moodboard} />
+
+            {/* <Route exact path="/music-daily" component={MusicDaily} />
+            <Route exact path="/yoga-video" component={YogaVideo} />
+            <Route exact path="/meditation-video" component={MeditationVideo} />  */}
           </Switch>
         </div>
       );
--- src/components/Main/Moodboard.js
+++ src/components/Main/Moodboard.js
@@ -1,6 +1,34 @@
-//import Sentence from './Sentence';
+import MusicDaily from '../Suggestions/MusicDaily';
+import YogaVideo from '../Suggestions/Yoga';
+import MeditationVideo from '../Suggestions/Meditation';
+import Inspiration from '../Suggestions/Inspiration'
 
 
-                // <div> 
-                // <Sentence />
-                // <div></div>
+
+import Sentence from './Sentence';
+import React from 'react';
+class Moodboard extends React.Component {
+    render () {
+    return (
+        <div>
+            <h3>Welcome to your Moodboard</h3>
+            <div> 
+          <Sentence />
+            </div>
+            <div> 
+          <YogaVideo />
+            </div>
+            <div> 
+          <MeditationVideo />
+            </div>
+            <div> 
+          <MusicDaily />
+            </div>
+            <div> 
+          <Inspiration />
+            </div>
+        </div>
+    )
+}
+}
+export default Moodboard;
