a703a1fa1a47e011c70390fc3c96de5e3b55c2e2
Salome Charpignon <salome.charpignon@gmail.com>
2020-12-10 12:45:18 +0000

    commit

--- src/components/Suggestions/EvasionRoom1.js
+++ src/components/Suggestions/EvasionRoom1.js
@@ -3,38 +3,61 @@ import './EvasionRoom.css';
 import 'video-react/dist/video-react.css';
 import AudioPlayer from 'react-h5-audio-player';
 import PlayerVideoReact from './VideoPlayer';
-import Progress from 'react-circle-progress-bar';
 
 class EvasionRoom1 extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    
+        this.state = { 
+            loop: true,
+            showCircle: false
+        }
+    }
 
-   
+    triggerCircle() {
+        this.setState({
+            showCircle: true
+          });
+    }
 
-    render() {
-
+    render () {
         return(
             <div>
+                { this.state.showCircle && <div class="breathing-circle"></div> }
+                
+          
+           
             
-                <div>
-                <PlayerVideoReact videoSrc={`https://res.cloudinary.com/dtu8rcnmp/video/upload/v1607446126/project3/Room1_xdpwwh.mp4`} />
-                </div>
-                <div>
+                <div className="audio-player-evasion">
                 <AudioPlayer className="audio-compo"
                     src={"https://res.cloudinary.com/dtu8rcnmp/video/upload/v1607453494/project3/Room1_sxd3ww.mp3"}
                     onPlay={e => console.log("onPlay")}
-                    showSkipControls="true"
-                    customAdditionalControls={[]}
-                    loop="true"
-                    />
+                    showSkipControls={"false"}
+                    // customAdditionalControls={[]}
+                    // loop={"true"}
+                    showJumpControls={"false"}
+                    showDownloadProgress={"false"}
+                    // showFilledProgress={"false"}
+                    // showFilledVolume={"true"}
+                     />
                 </div>
+
+               <div>
+                
+                </div>
+
                 <div>
-                 <Progress progress={75} className='progress-circle'/>
+                <PlayerVideoReact trigger={this.triggerCircle.bind(this)} videoSrc={`https://res.cloudinary.com/dtu8rcnmp/video/upload/v1607446126/project3/Room1_xdpwwh.mp4`} loop={"true"} />
                 </div>
+
+                
 
             </div>
             
         )
-    }
 }
+}
+
 
 export default EvasionRoom1;
 
